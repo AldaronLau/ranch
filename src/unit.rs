8c15f674b43bcb240794d57b7d5a8c3afe167ece
Jeryn Aldaron Lau <aldaronlau@gmail.com>
2026-03-08 18:25:21 -0500

    Generate unit types with macros

--- src/unit.rs
+++ src/unit.rs
@@ -5,55 +5,25 @@
 //! integer.
 //!
 //! Unit types can be constructed with [`Default::default()`] or their `new()`
-//! method if `const` is required.
+//! method if `const` is required.  Conversions between these unit types and
+//! [`prim@unit`] are also provided.
 
 use crate::*;
 
-/// [`i8`] that's guaranteed to be a specific value
-pub type UnitI8<const VAL: i8> = RangedI8<VAL, VAL>;
-/// [`i16`] that's guaranteed to be a specific value
-pub type UnitI16<const VAL: i16> = RangedI16<VAL, VAL>;
-/// [`i32`] that's guaranteed to be a specific value
-pub type UnitI32<const VAL: i32> = RangedI32<VAL, VAL>;
-/// [`i64`] that's guaranteed to be a specific value
-pub type UnitI64<const VAL: i64> = RangedI64<VAL, VAL>;
-/// [`i128`] that's guaranteed to be a specific value
-pub type UnitI128<const VAL: i128> = RangedI128<VAL, VAL>;
+macro_rules! unit {
+    ($type:ident, $nonzero:ident, $ranged:ident, $rnz:ident, $p:ty $(,)?) => {
+        #[doc = concat!("[`", stringify!($p), "`]")]
+        /// that's guaranteed to be a specific value
+        pub type $type<const VAL: $p> = $ranged<VAL, VAL>;
 
-/// [`u8`] that's guaranteed to be a specific value
-pub type UnitU8<const VAL: u8> = RangedU8<VAL, VAL>;
-/// [`u16`] that's guaranteed to be a specific value
-pub type UnitU16<const VAL: u16> = RangedU16<VAL, VAL>;
-/// [`u32`] that's guaranteed to be a specific value
-pub type UnitU32<const VAL: u32> = RangedU32<VAL, VAL>;
-/// [`u64`] that's guaranteed to be a specific value
-pub type UnitU64<const VAL: u64> = RangedU64<VAL, VAL>;
-/// [`u128`] that's guaranteed to be a specific value
-pub type UnitU128<const VAL: u128> = RangedU128<VAL, VAL>;
+        #[doc = concat!("[`", stringify!($p), "`]")]
+        /// that's guaranteed to be a specific non-zero value
+        pub type $nonzero<const VAL: $p> = $rnz<VAL, VAL>;
 
-/// [`i8`] that's guaranteed to be a specific non-zero value
-pub type UnitNonZeroI8<const VAL: i8> = RangedNonZeroI8<VAL, VAL>;
-/// [`i16`] that's guaranteed to be a specific non-zero value
-pub type UnitNonZeroI16<const VAL: i16> = RangedNonZeroI16<VAL, VAL>;
-/// [`i32`] that's guaranteed to be a specific non-zero value
-pub type UnitNonZeroI32<const VAL: i32> = RangedNonZeroI32<VAL, VAL>;
-/// [`i64`] that's guaranteed to be a specific non-zero value
-pub type UnitNonZeroI64<const VAL: i64> = RangedNonZeroI64<VAL, VAL>;
-/// [`i128`] that's guaranteed to be a specific non-zero value
-pub type UnitNonZeroI128<const VAL: i128> = RangedNonZeroI128<VAL, VAL>;
+        unit!($type, $p);
+        unit!($nonzero, $p);
+    };
 
-/// [`u8`] that's guaranteed to be a specific non-zero value
-pub type UnitNonZeroU8<const VAL: u8> = RangedNonZeroU8<VAL, VAL>;
-/// [`u16`] that's guaranteed to be a specific non-zero value
-pub type UnitNonZeroU16<const VAL: u16> = RangedNonZeroU16<VAL, VAL>;
-/// [`u32`] that's guaranteed to be a specific non-zero value
-pub type UnitNonZeroU32<const VAL: u32> = RangedNonZeroU32<VAL, VAL>;
-/// [`u64`] that's guaranteed to be a specific non-zero value
-pub type UnitNonZeroU64<const VAL: u64> = RangedNonZeroU64<VAL, VAL>;
-/// [`u128`] that's guaranteed to be a specific non-zero value
-pub type UnitNonZeroU128<const VAL: u128> = RangedNonZeroU128<VAL, VAL>;
-
-macro_rules! impl_default {
     ($type:ident, $p:ty $(,)?) => {
         impl<const VAL: $p> Default for $type<VAL> {
             fn default() -> Self {
@@ -86,26 +56,26 @@ macro_rules! impl_default {
     };
 }
 
-impl_default!(UnitI8, i8);
-impl_default!(UnitI16, i16);
-impl_default!(UnitI32, i32);
-impl_default!(UnitI64, i64);
-impl_default!(UnitI128, i128);
+unit!(UnitI8, UnitNonZeroI8, RangedI8, RangedNonZeroI8, i8);
+unit!(UnitI16, UnitNonZeroI16, RangedI16, RangedNonZeroI16, i16);
+unit!(UnitI32, UnitNonZeroI32, RangedI32, RangedNonZeroI32, i32);
+unit!(UnitI64, UnitNonZeroI64, RangedI64, RangedNonZeroI64, i64);
+unit!(
+    UnitI128,
+    UnitNonZeroI128,
+    RangedI128,
+    RangedNonZeroI128,
+    i128,
+);
 
-impl_default!(UnitU8, u8);
-impl_default!(UnitU16, u16);
-impl_default!(UnitU32, u32);
-impl_default!(UnitU64, u64);
-impl_default!(UnitU128, u128);
-
-impl_default!(UnitNonZeroI8, i8);
-impl_default!(UnitNonZeroI16, i16);
-impl_default!(UnitNonZeroI32, i32);
-impl_default!(UnitNonZeroI64, i64);
-impl_default!(UnitNonZeroI128, i128);
-
-impl_default!(UnitNonZeroU8, u8);
-impl_default!(UnitNonZeroU16, u16);
-impl_default!(UnitNonZeroU32, u32);
-impl_default!(UnitNonZeroU64, u64);
-impl_default!(UnitNonZeroU128, u128);
+unit!(UnitU8, UnitNonZeroU8, RangedU8, RangedNonZeroU8, u8);
+unit!(UnitU16, UnitNonZeroU16, RangedU16, RangedNonZeroU16, u16);
+unit!(UnitU32, UnitNonZeroU32, RangedU32, RangedNonZeroU32, u32);
+unit!(UnitU64, UnitNonZeroU64, RangedU64, RangedNonZeroU64, u64);
+unit!(
+    UnitU128,
+    UnitNonZeroU128,
+    RangedU128,
+    RangedNonZeroU128,
+    u128,
+);
